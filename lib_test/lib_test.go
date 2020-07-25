8968aedddbc1daae2df4520bdaae831ac85d12dc
ayoul3 <ayoul3.zos@gmail.com>
2020-07-25 10:18:10 +0200

    fix test

--- lib_test/lib_test.go
+++ lib_test/lib_test.go
@@ -9,12 +9,6 @@ import (
 var _ = Describe("Loading", func() {
 
 	Describe("NewBinaryFromDisk", func() {
-		Context("When file is on disk", func() {
-			It("should not trigger error", func() {
-				_, err := lib.NewBinaryFromDisk("../res/sock_file.exe")
-				Expect(err).ToNot(HaveOccurred())
-			})
-		})
 		Context("When file is not PE", func() {
 			It("should trigger error", func() {
 				_, err := lib.NewBinaryFromDisk("./lib_test.go")
